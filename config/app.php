72ad3b0ac086211febafa326810cb9c9600f4f70
HamidRezaAshkiyan <HamidRezaAshkiyan@gmail.com>
2021-02-26 12:59:36 +0330

    🌐 Fix Commit Message
    just changed version number to fix commit message

--- config/app.php
+++ config/app.php
@@ -11,7 +11,7 @@ return [
     | or any other location as required by the application or its packages.
     */
 
-    'version' => '0.1.0',
+    'version' => '0.1.1',
 
     /*
     |--------------------------------------------------------------------------
