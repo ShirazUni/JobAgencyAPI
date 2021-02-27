68a9ad5c991abd6d440c89e4912fdb8c19535eeb
HamidRezaAshkiyan <HamidRezaAshkiyan@gmail.com>
2021-02-27 21:26:00 +0330

    🌱 Added RequestSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UserTableSeeder::class);
+        $this->call(EmployerTableSeeder::class);
+        $this->call(EmployeeTableSeeder::class);
+        $this->call(RequestTableSeeder::class);
     }
 }
---
+++ database/seeders/RequestTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Request;
+use Faker\Factory;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class RequestTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Factory::create();
+
+        $password = Hash::make('password');
+
+        for ($i = 0; $i < 50; $i++) {
+            Request::create([
+                'employee_id'         => $faker->numberBetween(1, 50),
+                'employer_id'         => $faker->numberBetween(1, 50),
+                'request_description' => $faker->text,
+                'is_accepted'         => $faker->boolean
+            ]);
+        }
+    }
+}
